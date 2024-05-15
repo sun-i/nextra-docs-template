e22f3554181b306f87d52207d98276cdcf1c243c
sun <xinsun.cs@gmail.com>
2024-05-15 13:10:34 +0800

    添加、仓库地址、访问量统计

--- theme.config.tsx
+++ theme.config.tsx
@@ -20,6 +20,7 @@ const config: DocsThemeConfig = {
       <meta property="og:title" content="青砖生存手册" />
       <meta property="og:description" content="青山湖带砖生存指南" />
       <link rel="icon" href="https://cdn.jsdelivr.net/gh/sun-i/pic/redefine-avatar.svg" />
+      <script defer src="https://umami.csun.site/script.js" data-website-id="61c463b5-a55f-4da6-ab52-9fde8e1eee0f"></script>
     </>
   ),
   editLink: {
@@ -27,6 +28,17 @@ const config: DocsThemeConfig = {
   },
   feedback: {
     content: null,
+  },
+
+  project: {
+    link: 'https://github.com/sun-i/qingzhuan/'
+  },
+
+  chat: {
+    link: 'https://umami.csun.site/share/mCwsCb9MuwapX5Kz/docs.csun.site',
+    icon: (
+      <img src="https://img.csun.site/file/a41a73a169971278db6c2.png" alt="description" width="24" height="24" />
+    )
   }
 }
 
